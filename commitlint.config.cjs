42e10716327d88e61ce99779cef7dac6e321e604
fredheir <rolf@markolo.eu>
2026-08-06 11:33:40 +0300

    fix(ci): scope Dependabot commitlint exemption (#105)

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,10 +1,5 @@
-const isDependabotCommit = (message) =>
-  message.startsWith("chore(deps): bump ") &&
-  message.includes("Signed-off-by: dependabot[bot]");
-
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  ignores: [isDependabotCommit],
   rules: {
     "body-max-line-length": [2, "always", 200],
   },
